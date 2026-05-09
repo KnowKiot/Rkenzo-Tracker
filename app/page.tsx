7901db967a75247486b4e01142adc4d88fe0448c
KnowKiot <kyle8300fm@gmail.com>
2026-05-09 20:53:07 +0100

    Changed colours

--- app/page.tsx
+++ app/page.tsx
@@ -17,7 +17,7 @@ const ERAS = [
     name: 'Momentary Bliss',
     image: '/images/MB.jpeg',
     description: 'Upcoming Album expected to release 2026, Momentary Bliss has fans anticipating the release of grails such as Alone Now and more production from FinniX!Beats. Album seems to experiment with different sounds',
-    accent: '#abf5ff', // purple
+    accent: '#60ecff', // purple
     accentBg: 'rgba(167,139,250,0.08)',
   },
   {
@@ -31,7 +31,7 @@ const ERAS = [
     name: 'One Shot To Shine',
     image: '/images/OS2S.png',
     description: 'Rkenzos debut EP which put him on the map. One Shot To Shine released 23 August 2024 contains multiple fan favourites such as Papercuts, Out Til late, One Shot To Shine and many more',
-    accent: '#ffd900e7', // orange
+    accent: '#ffd900', // orange
     accentBg: 'rgba(251,146,60,0.08)',
   },
   {
